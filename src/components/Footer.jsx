91e07aae0786b1f518f090ccf2a140858b3e463c
Kibieptr <138297239+Lynnxha@users.noreply.github.com>
2024-01-14 02:06:56 -0800

    update: content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="w-full text-center py-7 bg-emerald-600 text-white">
+    <footer className="w-full text-center py-7 bg-emerald-600 text-white text-xs">
       <p>© {currentYear} i-Recite. All Rights Reserved ♥ by Kibieptr</p>
     </footer>
   );
